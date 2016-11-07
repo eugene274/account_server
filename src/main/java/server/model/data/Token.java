a710fd2d8fb013e14629ce969961107ee0711f22
Ivan Sivak <ivan@sivak.tech>
2016-11-07 12:44:58 +0300

    TokenHibernate done and tested

--- src/main/java/server/model/data/Token.java
+++ src/main/java/server/model/data/Token.java
@@ -1,22 +1,51 @@
 package server.model.data;
 
+import javax.persistence.*;
 import java.util.Calendar;
 import java.util.Random;
 
 /**
  * Created by eugene on 10/13/16.
  */
-@SuppressWarnings("DefaultFileTemplate")
+// @SuppressWarnings("DefaultFileTemplate")
+@Entity(name = "Tokens")
+@Table(name = "tokens")
 public class Token {
+    @Id
+    @Column(name = "token_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "token_string")
     private String tokenString;
+
+    @Column(name = "time_create")
     private Calendar createdAt;
+
+    @Column(name = "last_request")
     private Calendar lastRequestAt;
+
+    @Column(name = "active")
     private boolean active = true;
 
-    // TODO:
-    // @Embedded
-    UserProfile user;
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 
     private static Random generator = new Random();
 
@@ -31,6 +60,13 @@ public class Token {
 
 
     public Token() {
+        this.tokenString = generateToken();
+        this.createdAt = Calendar.getInstance();
+        this.lastRequestAt = createdAt;
+    }
+
+    public Token(Long userId) {
+        this.userId = userId;
         this.tokenString = generateToken();
         this.createdAt = Calendar.getInstance();
         this.lastRequestAt = createdAt;
